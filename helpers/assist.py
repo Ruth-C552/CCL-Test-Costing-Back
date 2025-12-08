055384d0916f71a581a62a6f7c05b109033c0b90
evans.nkole <evans.nkole@cidrz.org>
2025-12-08 14:38:24 +0200

    first commit

---
+++ helpers/assist.py
@@ -0,0 +1,166 @@
+import hashlib
+from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from zoneinfo import ZoneInfo
+import calendar
+
+INFOBIP_API_URL = "https://xk85nl.api.infobip.com/whatsapp/1/message/template"
+INFOBIP_API_TOKEN = (
+    "05704a467eaab51ea1bd2aabaa652517-c006af8e-55e6-4254-aa46-440344a6e040"
+)
+INFOBIP_PHONE_NUMBER = "12098869548"
+
+USER_MEMBER = 1
+USER_ADMIN = 2
+
+UPLOAD_DIR = "uploads"
+
+SECRET_KEY = "your-secret-key"
+ALGORITHM = "HS256"
+CURRENT_TIME_ZONE = "Africa/Lusaka"
+
+STATUS_DRAFT = 1
+STATUS_SUBMITTED = 2
+STATUS_APPROVED = 4
+STATUS_REJECTED = 5
+
+TRANSACTION_SAVINGS = 1
+TRANSACTION_SHARE = 2
+TRANSACTION_LOAN = 3
+TRANSACTION_LOAN_PAYMENT = 4
+TRANSACTION_INTEREST_CHARGED = 5
+TRANSACTION_INTEREST_PAID = 6
+TRANSACTION_SOCIAL_FUND = 7
+TRANSACTION_PENALTY_CHARGED = 8
+TRANSACTION_PENALTY_PAID = 9
+TRANSACTION_GROUP_EARNING = 10
+TRANSACTION_GROUP_EXPENSE = 11
+
+STATE_OPEN = 1
+STATE_CLOSED = 2
+
+REVIEW_ACTION_REJECT = 1
+REVIEW_ACTION_APPROVE = 2
+
+APPROVAL_STAGE_DRAFT = 1
+APPROVAL_STAGE_SUBMITTED = 2
+APPROVAL_STAGE_PRIMARY = 3
+APPROVAL_STAGE_SECONDARY = 4
+APPROVAL_STAGE_GUARANTOR = 5
+APPROVAL_STAGE_POP_UPLOAD = 6
+APPROVAL_STAGE_POP_APPROVAL = 7
+APPROVAL_STAGE_APPROVED = 8
+
+RESPONSE_NO = 1
+RESPONSE_YES = 2
+
+PENALTY_LATE_POSTING = 1
+PENALTY_MISSED_MEETING = 2
+PENALTY_LATE_MEETING = 3
+
+
+NOTIFY_WAITING = 1
+NOTIFY_SENT = 2
+
+
+def get_safe_name(input: str):
+    return input.replace(" ", "")
+
+
+def get_current_date(date=True):
+    # Set your timezon
+    tz = ZoneInfo(CURRENT_TIME_ZONE)
+
+    # Get current date with timezone
+    now = datetime.now(tz)
+
+    if date:
+        now = datetime(now.year, now.month, now.day)
+
+    return now
+
+
+def get_current_period():
+    # Set your timezon
+    date = get_current_date()
+    return get_date_period(date)
+
+def get_date_period(date):
+    return date.strftime("%Y%m")
+
+def get_first_month_day(inputDate=None):
+    # Set your timezon
+    tz = ZoneInfo(CURRENT_TIME_ZONE)
+
+    # Get current date with timezone
+    now = datetime.now(tz) if inputDate == None else inputDate
+
+    # Get first day of current month
+    first_day = datetime(now.year, now.month, 1, tzinfo=tz)
+
+    return first_day
+
+
+def get_last_month_day(inputDate=None):
+    # Set your timezon
+    tz = ZoneInfo(CURRENT_TIME_ZONE)
+
+    # Get current date with timezone
+    now = datetime.now(tz) if inputDate == None else inputDate
+
+    # Get last day of current month
+    last_day_num = calendar.monthrange(now.year, now.month)[1]
+    last_day = datetime(now.year, now.month, last_day_num, tzinfo=tz)
+
+    return last_day
+
+
+def encode_sha256(input):
+    """
+    Encodes the specified input to SHA-256
+
+    Args:
+        input (string): The input to encode.
+
+
+    Returns:
+        string: The encode string in SHA-256 format.
+    """
+    # Create a SHA-256 hash object
+    hasher = hashlib.sha256()
+    # Update the hash object with the byte-encoded message
+    hasher.update(input.encode("utf-8"))
+    # Get the hexadecimal representation of the hash
+    sha256_result = hasher.hexdigest()
+    return sha256_result
+
+
+# Create a password context using bcrypt
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+
+
+def hash_password(password: str) -> str:
+    """
+    Hash a plain password using bcrypt
+
+    Args:
+        passwrd (string): The password to hash.
+
+    Returns:
+        string: The hashed password string.
+    """
+    hashed = pwd_context.hash(password)
+    return hashed
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """
+    Verify a plain password against a hash
+
+    Args:
+        plain_password (string): The plain password to verify.
+        hashed_password (string): The password hash to verify against.
+    Returns:
+        bool: Returns true if the password matches the hash, false otherwise
+    """
+    return pwd_context.verify(plain_password, hashed_password)
---
+++ main.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI
+from database import engine, Base
+from routes import auth_routes
+from routes import user_routes
+from routes import bench_routes
+
+from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from helpers.http_client import init_client, close_client
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup logic
+    print("Application starting up new...")
+    yield
+    # Shutdown logic
+    print("Application shutting down new...")
+
+origins = [
+        "http://localhost",
+        "http://localhost:5173",
+        "https://your-frontend-domain.com",
+    ]
+
+
+app = FastAPI(title="CCL Test Costing [FastAPI/PostgreSQL]")
+
+app.mount("/static", StaticFiles(directory="uploads"), name="static")
+
+#COR
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],  # Allows all HTTP methods
+        allow_headers=["*"],  # Allows all headers
+    )
+
+# include routers
+app.include_router(user_routes.router)
+app.include_router(auth_routes.router)
+app.include_router(bench_routes.router)
+# create tables at startup
+
+
+@app.on_event("startup")
+async def startup():
+    await init_client()
+    async with engine.begin() as conn:
+        print("Application starting up old...")
+        await conn.run_sync(Base.metadata.create_all)
+        
+@app.on_event("shutdown")
+async def shutdown_event():
+    await close_client()
+    
+def get_httpsx_client():
+    return app.state.client
